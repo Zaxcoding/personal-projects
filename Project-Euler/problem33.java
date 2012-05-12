dbac9bf21576944f4a2fdd82fe71cd190b453c44
Zach Sadler <zaxcoding@gmail.com>
2012-05-12 12:17:51 -0400

    Solved problem 33!
    Did this one by hand.

---
+++ Project-Euler/problem33.java
@@ -0,0 +1,17 @@
+/* Zach Sadler
+ * zaxcoding@gmail.com
+ * Problem 33
+ * There are exactly four non-trivial examples of this type of fraction, 
+ * less than one in value, and containing two digits in the numerator and denominator.
+ */
+
+public class problem33
+{ 
+/*
+	Solved by hand in about five minutes.
+	16/64
+	49/98
+	19/95
+	26/65
+*/	
+}
